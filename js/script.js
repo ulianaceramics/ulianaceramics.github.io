e985a32db2d42a7eb1f05fb15d05eddf30fb36ab
Vlad Ivashchenko <mr.ruryks@gmail.com>
2020-01-05 22:30:20 +0200

    AddUpbutton

--- js/script.js
+++ js/script.js
@@ -1,14 +1,3 @@
-var header = document.querySelector('.header');
-var paralax = document.querySelector('.paralax');
-var navigation = document.querySelector('nav');
-window.addEventListener("scroll", parallax);
-
-function parallax(event) {
-    let offset = window.pageXOffset;
-    header.style.backgroundPositionY = offset + `px`;
-    paralax.style.backgroundPositionY = offset + `px`;
-}
-
 //Telegram bot
 var submit = document.querySelector('#submit_btn');
 submit.addEventListener('click', sendForm);
@@ -17,11 +6,29 @@ function sendForm() {
     let lastName = document.querySelector('#lastName').value;
     let phone = document.querySelector('#phone').value;
     let email = document.querySelector('#email').value;
-
+    let userQuestion = document.querySelector('#userQuestion').value;
     const token = "1025254186:AAG4f-ymQTmEbtON1PLHTC7ayrpAA2u-kTU";
-    let url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=1025254186&text=`;
+    const chatId = "-1001308747027";
+    let url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=`;
 
     let xhttp = new XMLHttpRequest();
-    xhttp.open("GET", url + `Hello Ulyana, you have a new client. Name: ${name}, LastName: ${lastName}, Phone: ${phone}, Email: ${email}. Please contact him.`, true);
+    xhttp.open("GET", url + `Hello Ulyana, you have a new client. Name: ${name}, LastName: ${lastName}, Phone: ${phone}, Email: ${email}. His question is : "${userQuestion}" Please contact him.`, true);
     xhttp.send();
 }
+
+// Smooth transition
+$(document).ready(function () {
+    $("nav").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top }, 1500);
+    });
+    $("#up").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top }, 1500);
+    });
+});
+
